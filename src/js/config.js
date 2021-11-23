1be2240875a97739149abd6c3643e5f90e4b2e7f
Joy-3005 <jyotirmoykarmakar02@gmail.com>
2021-11-23 23:50:32 +0530

    Initial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '75dc4080-485b-4a20-85ff-d3f8a1daf213';
+export const MODAL_CLOSE_SEC = 2.5;
---
+++ src/js/views/resultsView.js
@@ -0,0 +1,39 @@
+import View from './View.js';
+import icons from 'url:../../img/icons.svg';
+
+class ResultsView extends View {
+  _parentElement = document.querySelector('.results');
+  _errorMessage = 'No recipes found for your query!';
+  _successMessage = '';
+
+  _generateMarkup() {
+    return this._data.map(this._generateMarkupPreview).join('');
+  }
+
+  _generateMarkupPreview(res) {
+    const id = window.location.hash.slice(1);
+
+    return `
+      <li class="preview">
+        <a class="preview__link ${
+          res.id === id ? 'preview__link--active' : ''
+        }" href="#${res.id}">
+          <figure class="preview__fig">
+            <img src="${res.image}" alt="Test" />
+          </figure>
+          <div class="preview__data">
+            <h4 class="preview__title">${res.title}</h4>
+            <p class="preview__publisher">${res.publisher}</p>
+            <div class="preview__user-generated ${res.key ? '' : 'hidden'}">
+                <svg>
+                  <use href="${icons}#icon-user"></use>
+                </svg>
+            </div>
+          </div>
+        </a>
+      </li>
+    `;
+  }
+}
+
+export default new ResultsView();
